81baada4af9418fae0b1d37a9b0d5aee8b8dfb21
Christian Gendreau <christiangendreau@gmail.com>
2022-04-13 11:52:00 -0400

    Allow to use the body of the POST to send json for PDF

--- src/main/java/dina/api/requests/CreatePDF.java
+++ src/main/java/dina/api/requests/CreatePDF.java
@@ -10,15 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import dina.LabelCreator.LabelCreator;
 import dina.LabelCreator.Options.Options;
+import org.apache.commons.lang3.StringUtils;
 import spark.Request;
 import spark.Response;
 
 public class CreatePDF {
 
 	public String template;
-	private Options op;
-	private Request req;
-	private Response res;
+	private final Options op;
+	private final Request req;
+	private final Response res;
 
 	private List<String> codesForCleanUp = new ArrayList<>();
 	
@@ -30,13 +31,17 @@ public class CreatePDF {
 
 	public HttpServletResponse result() throws IOException {
 		   
-			if(req.queryParams("template")!=null)
-				op.templateFile = op.templateDir+"/"+req.queryParams("template");
-		
-		   LabelCreator labels = new LabelCreator(op, req.queryParams("data"));
-		   labels.baseURL = op.baseURL;
+			if(req.queryParams("template")!=null) {
+				op.templateFile = op.templateDir + "/" + req.queryParams("template");
+			}
+
+			// Try to get it from query params and if no there use the request body
+		  LabelCreator labelCreator = StringUtils.isNotBlank(req.queryParams("data")) ?
+					new LabelCreator(op, req.queryParams("data")) : new LabelCreator(op, req.body());
+
+		  labelCreator.baseURL = op.baseURL;
 		   //labels.baseURL = "http://"+req.host()+req.pathInfo();
-     	   labels.createPDF();
+		  labelCreator.createPDF();
      	   
      	   if(op.debug)
      		   System.out.println("Reading PDF: "+Paths.get(op.outputFile));
